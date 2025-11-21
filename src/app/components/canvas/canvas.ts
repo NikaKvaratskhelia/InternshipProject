c41b61289ec94e5c85daba5f65d7284a7cfd932b
NikaKvaratskhelia <nikakvaratskhelia13@gmail.com>
2025-11-21 22:25:41 +0400

    added details page

--- src/app/components/canvas/canvas.ts
+++ src/app/components/canvas/canvas.ts
@@ -1,7 +1,18 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  modelPath: string;
+  photos: string[];
+  cameraPos: number[];
+}
 
 @Component({
   selector: 'app-canvas',
@@ -11,11 +22,59 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 })
 export class Canvas implements AfterViewInit {
   @ViewChild('canvasRef', { static: true }) canvasRef!: ElementRef<HTMLCanvasElement>;
+
+  public jewelry: Product[] = [
+    {
+      id: 1,
+      name: 'Black Standart Ring',
+      description:
+        'Minimalist black-coated ring designed for everyday wear. Clean, modern, and built to complement any style without trying too hard.',
+      price: 24,
+      modelPath: 'Assets/Models/basicRing.glb',
+      photos: [
+        'Assets/images/blackRing.webp',
+        'Assets/images/blackRing1.webp',
+        'Assets/images/blackRing2.webp',
+      ],
+      cameraPos: [0, 0, 50],
+    },
+    {
+      id: 2,
+      name: 'Magic Ring',
+      description:
+        'A standout ring featuring detailed engravings and gemstone highlights. Crafted to look mystical without feeling cheap or gimmicky.',
+      price: 147,
+      modelPath: 'Assets/Models/magic_ring.glb',
+      photos: [
+        'Assets/images/magicRing.webp',
+        'Assets/images/magicRing2.webp',
+        'Assets/images/magicRing3.webp',
+      ],
+      cameraPos: [0, 0, 5],
+    },
+    {
+      id: 3,
+      name: 'Pearl Necklace',
+      description:
+        'Elegant freshwater-style pearls arranged in a classic strand. Soft shine, timeless look, and perfect for elevating any outfit.',
+      price: 85,
+      modelPath: 'Assets/Models/pearlNecklace.glb',
+      photos: [
+        'Assets/images/pearlNecklace.webp',
+        'Assets/images/pearlNecklace1.webp',
+        'Assets/images/pearlNecklace2.webp',
+      ],
+      cameraPos: [50, 50, 200],
+    },
+  ];
+
+  public id: number | null = null;
   private camera!: THREE.PerspectiveCamera;
   private scene!: THREE.Scene;
   private renderer!: THREE.WebGLRenderer;
   private orbitControls!: OrbitControls;
-  public path: string = '';
+  public path: string | undefined = '';
+  public wantedProduct: Product | undefined;
 
   ngAfterViewInit(): void {
     this.setUpScene();
@@ -23,7 +82,15 @@ export class Canvas implements AfterViewInit {
     this.loadModel();
   }
 
-  
+  constructor(private route: ActivatedRoute) {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    this.id = idParam ? +idParam : null;
+    if (this.id !== null) {
+      this.wantedProduct = this.jewelry.find((j) => j.id === this.id);
+    }
+
+    this.path = this.wantedProduct?.modelPath;
+  }
 
   private setUpScene() {
     const canvas = this.canvasRef.nativeElement;
@@ -37,7 +104,6 @@ export class Canvas implements AfterViewInit {
       0.1,
       1000
     );
-    this.camera.position.set(50, 50, 80);
     this.camera.lookAt(0, 0, 0);
 
     this.renderer = new THREE.WebGLRenderer({
@@ -70,16 +136,24 @@ export class Canvas implements AfterViewInit {
     const loader = new GLTFLoader();
 
     loader.load(
-      this.path,
+      this.path || '',
 
       (gltf) => {
         const model = gltf.scene;
 
         const box = new THREE.Box3().setFromObject(model);
         const center = new THREE.Vector3();
+        model.position.set(0, 0, 0);
         box.getCenter(center);
 
         model.position.sub(center);
+        if (this.wantedProduct?.cameraPos) {
+          this.camera.position.set(
+            this.wantedProduct?.cameraPos[0],
+            this.wantedProduct?.cameraPos[1],
+            this.wantedProduct?.cameraPos[2]
+          );
+        }
 
         this.scene.add(model);
       },
--- src/app/components/details/details.ts
+++ src/app/components/details/details.ts
@@ -1,12 +1,87 @@
 import { Component } from '@angular/core';
-import { Canvas } from "../canvas/canvas";
+import { Canvas } from '../canvas/canvas';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  modelPath: string;
+  photos: string[];
+}
 
 @Component({
   selector: 'app-details',
-  imports: [Canvas],
+  imports: [Canvas, CommonModule],
   templateUrl: './details.html',
   styleUrl: './details.scss',
 })
 export class Details {
+  public jewelry: Product[] = [
+    {
+      id: 1,
+      name: 'Black Standart Ring',
+      description:
+        'Minimalist black-coated ring designed for everyday wear. Clean, modern, and built to complement any style without trying too hard.',
+      price: 24,
+      modelPath: 'Assets/Models/basicRing.glb',
+      photos: [
+        'Assets/images/blackRing.webp',
+        'Assets/images/blackRing1.webp',
+        'Assets/images/blackRing2.webp',
+      ],
+    },
+    {
+      id: 2,
+      name: 'Magic Ring',
+      description:
+        'A standout ring featuring detailed engravings and gemstone highlights. Crafted to look mystical without feeling cheap or gimmicky.',
+      price: 147,
+      modelPath: 'Assets/Models/magic_ring.glb',
+      photos: [
+        'Assets/images/magicRing.webp',
+        'Assets/images/magicRing2.webp',
+        'Assets/images/magicRing3.webp',
+      ],
+    },
+    {
+      id: 3,
+      name: 'Pearl Necklace',
+      description:
+        'Elegant freshwater-style pearls arranged in a classic strand. Soft shine, timeless look, and perfect for elevating any outfit.',
+      price: 85,
+      modelPath: 'Assets/Models/pearlNecklace.glb',
+      photos: [
+        'Assets/images/pearlNecklace.webp',
+        'Assets/images/pearlNecklace1.webp',
+        'Assets/images/pearlNecklace2.webp',
+      ],
+    },
+  ];
 
+  public currentIndex = 0;
+  public id: number | null = null;
+  public wantedProduct: Product | undefined;
+  public wantedPhoto: 'gallery' | '3d' = 'gallery';
+
+  constructor(private route: ActivatedRoute) {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    this.id = idParam ? +idParam : null;
+    if (this.id !== null) {
+      this.wantedProduct = this.jewelry.find((j) => j.id === this.id);
+    }
+  }
+
+  prev() {
+    if (!this.wantedProduct) return;
+    this.currentIndex =
+      (this.currentIndex - 1 + this.wantedProduct.photos.length) % this.wantedProduct.photos.length;
+  }
+
+  next() {
+    if (!this.wantedProduct) return;
+    this.currentIndex = (this.currentIndex + 1) % this.wantedProduct.photos.length;
+  }
 }
