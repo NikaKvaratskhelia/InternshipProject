32da92fa960d7cae4f05042f5ac616700d056e51
NikaKvaratskhelia <nikakvaratskhelia13@gmail.com>
2025-11-22 16:02:48 +0400

    header footer added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Main } from './components/main/main';
 import { Details } from './components/details/details';
+import { Deposit } from './components/deposit/deposit';
+import { Cart } from './components/cart/cart';
 
 export const routes: Routes = [
   {
@@ -10,5 +12,13 @@ export const routes: Routes = [
   {
     path: 'details/:id',
     component: Details,
+  },
+  {
+    path: 'deposit',
+    component: Deposit,
+  },
+  {
+    path: 'cart',
+    component: Cart,
   },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Header } from "./components/header/header";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule,],
+  imports: [RouterModule, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
